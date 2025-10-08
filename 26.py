010703c6ef884780aa8d0d61e0f69a09110684a0
nitbig <118971749+nitbig@users.noreply.github.com>
2025-10-08 21:49:19 +0530

    ESCAPE SEQUENCES

---
+++ 26.py
@@ -0,0 +1,12 @@
+#Escape Sequences
+
+"""
+\n       Newline
+\t       Tab 
+\'       Single quote
+\"       Double quote
+\\       Backslash
+"""
+
+print("Hello\nWorld")  #Line Break
+print("Hello\tWorld")  # Tab Space
