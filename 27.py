ae5a491a0696bb95effe233f48b07a428d117c70
nitbig <118971749+nitbig@users.noreply.github.com>
2025-10-08 21:58:10 +0530

    RAW STRING

---
+++ 27.py
@@ -0,0 +1,7 @@
+#RAW STRINGS
+
+print(r"C:\new\folder")    #prints C:\new\folder
+
+# In a raw string, escape sequences are not processed.
+# You make a string “raw” by prefixing it with an r or R
+
