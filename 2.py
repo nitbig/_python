339d4b13550c547ad3c3d747829a667342db7a35
nitbig <118971749+nitbig@users.noreply.github.com>
2025-10-08 21:23:32 +0530

    BASIC 1

---
+++ 2.py
@@ -0,0 +1,5 @@
+#type conversion
+a=float("2")
+b=3.14
+print (type(a))
+print(a+b)
---
+++ 3.py
@@ -0,0 +1,5 @@
+#types/ of operators
+#arithmetic operator (+,-,*,/,%,**) double star is power
+#relational operator(>,<,==,!=,>=,<=)
+#assignment operator(=,+=,-=,*=,/=,%=,**=)
+#logical operator(and, or and not)
