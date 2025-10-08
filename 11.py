ea6ef29e3149117c82428184e43d75800fbc6ff2
nitbig <118971749+nitbig@users.noreply.github.com>
2025-10-08 21:38:39 +0530

    BASIC 3

---
+++ 11.py
@@ -0,0 +1,12 @@
+# take two vlaues from the user and find its average
+
+#input two floating point numbers from the user
+
+num1= float(input("Enter the first number:"))
+num2= float(input("Enter the second number:"))
+
+#calculate average
+average=(num1+num2)/2
+
+#print the average
+print("the average of the two numbers is:", average)
---
+++ 12.py
@@ -0,0 +1,9 @@
+#input two integers from the user
+a=int(input("Enter the first integer(a):"))
+b=int(input("Enter the second integer(b):"))
+
+#check is a is greater than or equal to b
+result=a>=b
+
+#print the result(True or False)
+print(result)
