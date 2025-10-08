ff223c6167bc8dda0efb099446e2f4a843dd38a0
nitbig <118971749+nitbig@users.noreply.github.com>
2025-10-08 22:03:24 +0530

    Conditional statements

---
+++ 29.py
@@ -0,0 +1,13 @@
+#Conditional Statements
+
+#Basic Syntax
+
+"""if condition:
+#code block if condition is true
+
+elif another_condition
+# code block if second condition is True
+
+else:
+#code block if all conditions are False
+"""
